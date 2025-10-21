e47055d51e3fc310a4ffc9982cb86e76aeff05fe
mmdomingues <matheusmdomingues55@gmail.com>
2025-10-21 18:30:50 -0300

    Enum

---
+++ src/main/java/com/projetoweb/projetospring/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.projetoweb.projetospring.services;
+
+import com.projetoweb.projetospring.entities.Order;
+import com.projetoweb.projetospring.entities.User;
+import com.projetoweb.projetospring.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = orderRepository.findById(id);
+        return obj.get();
+
+    }
+}
